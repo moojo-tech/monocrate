1004c89ee434eeddbec899f86fc2337c2f3c4abb
Itay Maman <94941+imaman@users.noreply.github.com>
2026-07-19 21:45:27 +0300

    vitest basic reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,16 +5,12 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     include: ['tests/**/*.test.ts'],
+    reporters: ['basic'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html', 'lcov'],
       include: ['src/**/*.ts'],
-      exclude: [
-        'src/**/*.d.ts',
-        'src/**/*.compilation-test.ts',
-        'src/main.ts',
-        'src/monocrate-cli.ts',
-      ],
+      exclude: ['src/**/*.d.ts', 'src/**/*.compilation-test.ts', 'src/main.ts', 'src/monocrate-cli.ts'],
       thresholds: {
         lines: 90,
         functions: 90,
