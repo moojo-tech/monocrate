692fa80f93dbe2467c6c3f1283f0651be9c03d63
Itay Maman <94941+imaman@users.noreply.github.com>
2026-07-20 23:13:52 +0300

    tagorder

--- src/npm-client.ts
+++ src/npm-client.ts
@@ -42,7 +42,7 @@ export class NpmClient {
   }
 
   async publish(tarballPath: string, tag?: string, passThroughArgs?: string[]): Promise<void> {
-    const args = [tarballPath, ...(tag ? ['--tag', tag] : []), ...(passThroughArgs ?? [])]
+    const args = [tarballPath, ...(passThroughArgs ?? []), ...(tag ? ['--tag', tag] : [])]
     await runNpm('publish', args, AbsolutePath(path.dirname(tarballPath)), { ...this.npmOptions, stdio: 'inherit' })
   }
 
