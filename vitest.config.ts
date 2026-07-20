d02f902314706de0329002dd5126a7adc9620e95
Itay Maman <94941+imaman@users.noreply.github.com>
2026-07-20 11:22:52 +0300

    concurrencty1

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    maxWorkers: 2,
+    minWorkers: 1,
     include: ['tests/**/*.test.ts'],
     reporters: ['basic'],
     coverage: {
