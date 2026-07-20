e1f44b479bfa54184745b89d79f7a4aafc47af5a
Itay Maman <94941+imaman@users.noreply.github.com>
2026-07-20 23:08:43 +0300

    reorder

--- src/monocrate.ts
+++ src/monocrate.ts
@@ -43,7 +43,7 @@ async function monocrateImpl(options: MonocrateOptions, dispenser: TempDirDispen
     throw new Error(`cwd is not a directory: ${cwd}`)
   }
   const dynamicImportsPolicy = options.dynamicImportsPolicy ?? 'allow'
-  const tarballsDir = path.resolve(options.packDestination ?? cwd, cwd)
+  const tarballsDir = path.resolve(cwd, options.packDestination ?? cwd)
   fs.mkdirSync(tarballsDir, { recursive: true })
   // Determine whether to use unified max version or individual versions per package
   const useMax = options.max ?? false
